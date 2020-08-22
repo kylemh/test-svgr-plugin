64563f204183f4ca47fa595eac359add92d5523f
Kyle Holmberg <inbox@kylemh.com>
2020-08-22 10:58:58 -0700

    fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Link from 'next/Link';
+import Link from 'next/link';
 import { ReactComponent as Noodle } from '../images/noodle.svg';
 import styles from 'styles/Home.module.css';
 
